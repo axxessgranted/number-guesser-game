0410df93512af32b129fba76a44878bd97f1c473
Grant Kantor <58207637+axxessgranted@users.noreply.github.com>
2022-05-13 17:57:46 +0200

    Limit textinput to numbers

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,11 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType={'number-pad'}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
