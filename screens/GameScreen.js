e5a69efba83bfb56094f1b8a5bfc005a24cdf4a2
Grant Kantor <58207637+axxessgranted@users.noreply.github.com>
2022-09-04 16:24:59 +0200

    Add Title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { View, Text, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 const GameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess!</Text>
+      <Title>Opponent's Guess</Title>
       {/* GUESS */}
       <View>
         <Text>Higher or Lower</Text>
